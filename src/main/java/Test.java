67de476a6e4f9d11f3a51e1153825f02962f8402
Ina Krasnokutska <inessa.krasnokutska@gmail.com>
2022-04-06 20:57:32 +0300

    First test

---
+++ src/main/java/Test.java
@@ -0,0 +1,11 @@
+public class Test {
+    public static void main(String[] args) {
+        // System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
+
+        // WebDriver driver = new ChromeDriver();
+
+        //  driver.get("https://google.com");
+
+        System.out.println("Hello world");
+    }
+}
